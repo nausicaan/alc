5cc0615fc5fded63352eb9c7718a104954857e34
Byron Stuike <byron.stuike@gov.bc.ca>
2023-05-11 15:49:31 -0700

    Add function for splitting urls

--- download.rb
+++ download.rb
@@ -8,24 +8,28 @@ $stdout.print %x[ansible-playbook playbook.yaml --tags=discover]
 l = "sources/missing.txt"
 e = File.readlines(l)
 
-def jenja(finds, dumpty)
+def jenja()
   iterate = 0
   value = ""
-  while iterate < finds
-    value = value + dumpty[iterate] + "/"
+  while iterate < @finds
+    value = value + @dumpty[iterate] + "/"
     iterate += 1
   end
   return value
 end
 
+def isolate(line)
+  @finds = line.count('/')
+  tolerate = @finds + 1
+  @dumpty = line.split('/', tolerate)
+  @humpty = jenja()
+  @humpty.chomp!
+  @dumpty[@finds].chomp!
+end
+
 e.each do |line|
-  finds = line.count('/')
-  tolerate = finds + 1
-  dumpty = line.split('/', tolerate)
-  humpty = jenja(finds, dumpty)
-  humpty.chomp!
-  dumpty[finds].chomp!
-  $stdout.print %x[ansible-playbook playbook.yaml --tags=download --extra-vars "nextlink='#{humpty}' file='#{dumpty[finds]}'"]
+  isolate("#{line}")
+  $stdout.print %x[ansible-playbook playbook.yaml --tags=download --extra-vars "nextlink='#{@humpty}' file='#{@dumpty[@finds]}'"]
 end
 
 mirage = File.exist?("results/succeed.txt")
@@ -34,9 +38,10 @@ if mirage
   y = "results/succeed.txt"
   z = File.readlines(y)
   z.each do |line|
+    isolate("#{line}")
     line.chomp!
     line[0..43] = ""
-    $stdout.print %x[mc cp --recursive ~/Documents/alc/files/assets/alc/assets/#{line} alc/assets/#{line[0..86]}]
-    # $stdout.puts "mc cp --recursive ~/Documents/alc/files/assets/alc/assets/#{line} alc/assets/#{line[0..86]}"
+    @humpty[0..43] = ""
+    $stdout.print %x[mc cp --recursive ~/Documents/alc/files/assets/alc/assets/#{line} alc/assets/#{@humpty}]
   end
 end
